cedc3b2340b7a320d2056d6e9946edb3b7694f9b
REDRAGON <148379059+REDRAGO14@users.noreply.github.com>
2026-08-17 14:38:16 -0700

    setup basic exprss server

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import dotenv from "dotenv"
 import path from "path"
+import express from "express"
 
 dotenv.config({
-    path: path.resolve(process.cwd(), ".env")
+   path: path.resolve(process.cwd(), ".env")
+})  
+
+let port = process.env.PORT || 3000
+let app = express()
+
+app.get("/", (req, res)=>{
+    res.send("welcome home")
 })
 
-let userName = process.env.name
-console.log("start of the backend");
-console.log("USER: ", userName);
+app.get("/about", (req, res) =>{
+    res.send("this is about")
+})
+
+
+app.listen(port, ()=>{
+   console.log( `server is listening on http://localhost:${port}`);
+})
