7aa6abdeddaed66f8b2787e503852b8bf5d9bbe0
REDRAGON <148379059+REDRAGO14@users.noreply.github.com>
2026-08-17 14:13:04 -0700

    add dotenv to the project

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+import dotenv from "dotenv"
+import path from "path"
+
+dotenv.config({
+    path: path.resolve(process.cwd(), ".env")
+})
+
+let userName = process.env.name
 console.log("start of the backend");
+console.log("USER: ", userName);
